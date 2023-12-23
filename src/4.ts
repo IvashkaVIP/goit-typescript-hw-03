13347f6c0b92be732fb0c166c2d585c9b6358140
IvashkaVIP <ivashka.co@gmail.com>
2023-12-23 16:03:29 +0200

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  
+    private signature: number;
+  
+    constructor() {
+    this.signature = Math.random();
+  }
+  
+    getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+ 
+    constructor(private key: Key) { }
+ 
+    getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+      this.door = false;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature === key.getSignature) this.door = true;
+    else this.door = false;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
